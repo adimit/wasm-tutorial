3579e5dea7dfd42fed16ea0539d465ab430cfe9a
Aleksandar Dimitrov <git@aleks.bg>
2020-07-23 22:27:00 +0200

    Implement GoL

--- src/gol.rs
+++ src/gol.rs
@@ -1,7 +1,16 @@
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Copy)]
 pub enum CellState {
-    ALIVE,
-    DEAD
+    DEAD = 0,
+    ALIVE = 1
+}
+
+impl CellState {
+    fn flip(&self) -> CellState {
+        match self {
+            CellState::ALIVE => CellState::DEAD,
+            CellState::DEAD => CellState::ALIVE
+        }
+    }
 }
 
 pub struct Universe {
@@ -9,8 +18,79 @@ pub struct Universe {
     cells: Vec<CellState>
 }
 
-fn tick(universe: &mut Vec<CellState>) {
+impl Universe {
+    fn print(&self) {
+        for (pos, cell) in self.cells.iter().enumerate() {
+            let c = if *cell == CellState::ALIVE { "█" } else { "░" };
+            let newline = if pos % self.edge_size == self.edge_size - 1 { "\n" } else { "" };
+            print!("{}{}", c, newline);
+        }
+    }
 
+    fn index(&self, x: usize, y: usize) -> usize {
+        (self.edge_size * (y % self.edge_size)) + (x % self.edge_size)
+    }
+
+    fn deindex(&self, index: usize) -> (usize, usize) {
+        let x = index % self.edge_size;
+        let y = index / self.edge_size;
+        (x, y)
+    }
+
+    fn flip(&mut self, x: usize, y: usize) {
+        let index = self.index(x, y);
+        self.flip_index(index);
+    }
+
+    fn flip_index(&mut self, index: usize) {
+        self.cells[index] = self.cells[index].flip();
+    }
+
+    fn get(&self, x: usize, y: usize) -> CellState {
+        self.cells[self.index(x, y)]
+    }
+
+    fn get_neighbour_indices(&self, index: usize) -> Vec<(usize,usize)> {
+        let (x, y) = self.deindex(index);
+        let row_above = if y == 0 { self.edge_size - 1 } else { y - 1 };
+        let row_below = if y == self.edge_size - 1 { 0 } else { y + 1 };
+        let column_to_the_left  = if x == 0 { self.edge_size - 1 } else { x - 1 };
+        let column_to_the_right = if x == self.edge_size - 1 { 0 } else { x + 1 };
+        vec![
+            (column_to_the_left, row_above),
+            (x, row_above),
+            (column_to_the_right, row_above),
+            (column_to_the_left, y),
+            (column_to_the_right, y),
+            (column_to_the_left, row_below),
+            (x, row_below),
+            (column_to_the_right, row_below)
+        ]
+    }
+
+    fn get_amount_of_live_neighbours(&self, index: usize) -> u8 {
+        self.get_neighbour_indices(index).iter().map(|(x, y)| { self.get(*x, *y) as u8 }).sum()
+    }
+
+    fn index_has_to_be_flipped(&self, index: usize) -> bool {
+        let live_neighbours = self.get_amount_of_live_neighbours(index);
+        match (self.cells[index], live_neighbours) {
+            (CellState::ALIVE, 0) => true,
+            (CellState::ALIVE, 1) => true,
+            (CellState::ALIVE, 2) => false,
+            (CellState::ALIVE, 3) => false,
+            (CellState::ALIVE, _) => true,
+            (CellState::DEAD, 3) => true,
+            (CellState::DEAD, _) => false
+        }
+    }
+
+    fn tick(&mut self) {
+        let flip_indices: Vec<usize> = (0..self.cells.len()).filter(|index| {
+            self.index_has_to_be_flipped(*index)
+        }).collect();
+        flip_indices.iter().for_each(|index| { self.flip_index(*index) });
+    }
 }
 
 pub fn build_universe(edge_size: usize) -> Universe {
@@ -19,7 +99,95 @@ pub fn build_universe(edge_size: usize) -> Universe {
 }
 
 #[test]
+fn get_amount_of_live_neighbours_works() {
+    let mut universe = build_universe(4);
+    assert_eq!(universe.get_amount_of_live_neighbours(0), 0);
+    universe.flip(0, 1);
+    assert_eq!(universe.get_amount_of_live_neighbours(0), 1);
+    universe.flip(0, 3);
+    assert_eq!(universe.get_amount_of_live_neighbours(0), 2);
+    universe.flip(1, 3);
+    assert_eq!(universe.get_amount_of_live_neighbours(0), 3);
+    universe.flip(3, 3);
+    assert_eq!(universe.get_amount_of_live_neighbours(0), 4);
+}
+
+#[test]
+fn get_neighbour_indices_wraps_correctly() {
+    let universe = build_universe(5);
+    assert_eq!(universe.get_neighbour_indices(0),
+               vec![(4, 4), (0, 4), (1, 4), (4, 0), (1, 0), (4, 1), (0, 1), (1, 1)]
+    );
+    assert_eq!(universe.get_neighbour_indices(24),
+               vec![(3, 3), (4, 3), (0, 3), (3, 4), (0, 4), (3, 0), (4, 0), (0, 0)]
+    );
+}
+
+#[test]
 fn build_universe_has_correct_size() {
     let universe = build_universe(5);
     assert_eq!(universe.cells.len(), 25);
+}
+
+#[test]
+fn get_with_large_index_wraps() {
+    let mut universe = build_universe(4);
+    universe.flip(4, 4);
+    assert_eq!(universe.get(0, 0), CellState::ALIVE);
+}
+
+#[test]
+fn universe_can_flip_cell() {
+    let mut universe = build_universe(2);
+    universe.flip(0, 1);
+    assert_eq!(universe.get(0, 1), CellState::ALIVE);
+}
+
+#[test]
+fn lone_cell_dies_in_one_tick() {
+    let mut universe = build_universe(3);
+    universe.flip(1,1);
+    universe.tick();
+    assert_eq!(universe.get(1,1), CellState::DEAD);
+}
+
+#[test]
+fn blocks_are_stable() {
+    let mut universe = build_universe(4);
+    universe.flip(0,0);
+    universe.flip(0,1);
+    universe.flip(1,1);
+    universe.flip(1,0);
+    universe.tick();
+    universe.tick();
+    universe.tick();
+    universe.tick();
+    universe.tick();
+
+    assert_eq!(universe.get(0,0), CellState::ALIVE);
+    assert_eq!(universe.get(1,1), CellState::ALIVE);
+    assert_eq!(universe.get(1,0), CellState::ALIVE);
+    assert_eq!(universe.get(0,1), CellState::ALIVE);
+    assert_eq!(universe.cells.iter().filter(|cell| { **cell == CellState::ALIVE }).count(), 4);
+}
+
+#[test]
+fn blinkers_are_stable() {
+    let mut universe = build_universe(5);
+
+    universe.flip(1,1);
+    universe.flip(1,2);
+    universe.flip(1,3);
+
+    universe.tick();
+
+    assert_eq!(universe.get(0,2), CellState::ALIVE);
+    assert_eq!(universe.get(1,2), CellState::ALIVE);
+    assert_eq!(universe.get(2,2), CellState::ALIVE);
+
+    universe.tick();
+
+    assert_eq!(universe.get(1,1), CellState::ALIVE);
+    assert_eq!(universe.get(1,2), CellState::ALIVE);
+    assert_eq!(universe.get(1,3), CellState::ALIVE);
 }
